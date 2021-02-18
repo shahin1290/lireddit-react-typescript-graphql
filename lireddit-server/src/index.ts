389cb4f701de2f1d1e818cafb3029ac7be11b7a0
shahin1290 <s.patowary@yahoo.com>
2021-02-18 22:06:07 +0100

    Apollo Server Express Setup

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -3,14 +3,31 @@ import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import dotenv from 'dotenv';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 dotenv.config();
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log('server listening on port 4000');
+  });
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
