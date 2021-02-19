88bf57ac9ab1678b5df8ccbcedc68f9df60bccff
shahin1290 <s.patowary@yahoo.com>
2021-02-19 15:14:30 +0100

    register and login user

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -9,6 +8,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 dotenv.config();
 
@@ -20,7 +20,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
